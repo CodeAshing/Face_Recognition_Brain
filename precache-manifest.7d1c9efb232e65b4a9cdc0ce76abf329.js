20f56914e6fba314e5a605ad8139d136bac48bb4
asharib90 <asharibahmed143@gmial.com>
2020-05-01 22:11:37 -0700

    Updates

---
+++ precache-manifest.7d1c9efb232e65b4a9cdc0ce76abf329.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53473a6ecbb3640279efa34aa641a1f3",
+    "url": "/Face_Recognition_Brain/index.html"
+  },
+  {
+    "revision": "77bcc95107d4a5477eb2",
+    "url": "/Face_Recognition_Brain/static/css/2.036b41f9.chunk.css"
+  },
+  {
+    "revision": "be810d9167e6fb266c18",
+    "url": "/Face_Recognition_Brain/static/css/main.a7fc6c9c.chunk.css"
+  },
+  {
+    "revision": "77bcc95107d4a5477eb2",
+    "url": "/Face_Recognition_Brain/static/js/2.e27b0fad.chunk.js"
+  },
+  {
+    "revision": "501f379103cbfcb5ff11211dd39a28a2",
+    "url": "/Face_Recognition_Brain/static/js/2.e27b0fad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be810d9167e6fb266c18",
+    "url": "/Face_Recognition_Brain/static/js/main.cdbb6ad4.chunk.js"
+  },
+  {
+    "revision": "d9bb8d17bbac1971888d",
+    "url": "/Face_Recognition_Brain/static/js/runtime-main.17d1f4f0.js"
+  }
+]);
